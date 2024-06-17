337c17e44b9043a849092f1d518daee9b74567d1
LilMat69 <semataoe@est.utn.ac.cr>
2024-06-17 15:30:38 -0600

    SCRUM-38: jwt_manager firstFase

---
+++ utils/jwt_manager.py
@@ -0,0 +1,34 @@
+import jwt
+from datetime import datetime, timedelta
+from decouple import config
+
+def generate_jwt(identity):
+    """
+    Generate a JSON Web Token (JWT) for the given identity
+    """
+    payload = {
+        'exp': datetime.utcnow() + timedelta(minutes=30),  # Token expires in 30 minutes
+        'iat': datetime.utcnow(),
+        'sub': identity
+    }
+    token = jwt.encode(payload, config('JWT_SECRET_KEY'), algorithm='HS256')
+    return token
+
+def validate_jwt(token):
+    """
+    Validate a JSON Web Token (JWT)
+    """
+    try:
+        payload = jwt.decode(token, config('JWT_SECRET_KEY'), algorithms=['HS256'])
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
+
+def get_jwt_identity(token):
+    """
+    Get the identity from a valid JWT
+    """
+    payload = jwt.decode(token, config('JWT_SECRET_KEY'), algorithms=['HS256'])
+    return payload['sub']
