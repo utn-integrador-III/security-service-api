7e7ff30157c0d51ff7401f134828d2151346a671
Remma27 <emrodriguezso@est.utn.ac.cr>
2024-06-28 15:12:56 -0600

    Cabios en endpoint de registro de usuarios

--- models/user/user.py
+++ models/user/user.py
@@ -1,18 +1,18 @@
 from pymongo import MongoClient
 from decouple import config
-from utils.encryption_utils import EncryptionUtil 
+from utils.encryption_utils import EncryptionUtil
 
 class UserModel:
     @staticmethod
     def create_user(user_data):
         encryption_util = EncryptionUtil()
         encrypted_password = encryption_util.encrypt(user_data['password'])
+        user_data['password'] = encrypted_password
 
         client = MongoClient(config('MONGO_URL'))
         db = client.get_database(config('MONGO_DB'))
         users_collection = db.get_collection(config('USER_COLLECTION'))
 
-        user_data['password'] = encrypted_password
         users_collection.insert_one(user_data)
     
     @staticmethod
