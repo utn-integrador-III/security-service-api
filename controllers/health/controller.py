88afe691368d666030c2b3da1c9ea05923f08f7c
chrisOMG33 <christopherce1918@gmail.com>
2024-07-07 00:22:54 -0600

    Document the endpoint to enroll a new user. In swagger.yml add the endpoint inside Auth section & the existing endpoint with the path /register was changed to the correct /auth/enrollment

--- controllers/health/controller.py
+++ controllers/health/controller.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask_restful import Resource
 
 from utils.server_response import *
@@ -22,4 +23,30 @@ class HealthController(Resource):
             print(ex)
             logging.exception(ex)
             return ServerResponse(message='Connection to DB is not possible.',
+=======
+from flask_restful import Resource
+
+from utils.server_response import *
+from utils.message_codes import *
+from models.health.model import HealthModel
+import logging
+
+
+class HealthController(Resource):
+    route = '/health'
+
+    """
+    Get heath
+    """
+    def get(self):
+        try:
+            # Check connection status
+            info_db= HealthModel.getInfoDB()
+            return ServerResponse(message='Connection to DB is OK',
+                                        message_code=HEALTH_SUCCESSFULLY, status=StatusCode.OK)         
+        except Exception as ex:
+            print(ex)
+            logging.exception(ex)
+            return ServerResponse(message='Connection to DB is not possible.',
+>>>>>>> 3700107 (Document the endpoint to enroll a new user. In swagger.yml add the endpoint inside Auth section & the existing endpoint with the path /register was changed to the correct /auth/enrollment)
                                       message_code=HEALTH_NOT_FOUND, status=StatusCode.NOT_FOUND)
--- db/mongo_client.py
+++ db/mongo_client.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from pymongo import MongoClient
 from decouple import config
 import logging
@@ -55,4 +56,63 @@ class Connection:
             return self.collection.find_one({'email': email})
         except Exception as e:
             logging.error(f"Database error in find_by_email: {str(e)}", exc_info=True)
+=======
+from pymongo import MongoClient
+from decouple import config
+import logging
+
+
+class Connection:
+
+    def __init__(self, collection_name):
+        self.collection = None
+        self.db = None
+        self.connect(collection_name)
+
+    def connect(self, collection_name):
+        uri = config('MONGO_URL')
+        db = config('MONGO_DB')
+        self.collection = MongoClient(uri)[db][collection_name]
+
+    def get_all_data(self):
+        try:
+            result = self.collection.find({})
+        except Exception as e:
+            return e
+        return result
+
+    def get_by_id(self, id):
+        try:
+            result = self.collection.find({'id':id})
+        except Exception as e:
+            return e
+        return result
+    
+    def create_data(self, data):
+        try:
+            return self.collection.insert_one(data)
+        except Exception as e:
+            return e
+
+    def update_data(self, id, new_deal_data):
+        try:
+            self.collection.update_one(
+                {"id": id},
+                {"$set": new_deal_data}
+            )
+        except Exception as e:
+            return e
+
+    def delete_data(self, id):
+        try:
+            return self.collection.delete_one({'id': id})
+        except Exception as e:
+            return e
+    
+    def find_by_email(self, email):
+        try:
+            return self.collection.find_one({'email': email})
+        except Exception as e:
+            logging.error(f"Database error in find_by_email: {str(e)}", exc_info=True)
+>>>>>>> 3700107 (Document the endpoint to enroll a new user. In swagger.yml add the endpoint inside Auth section & the existing endpoint with the path /register was changed to the correct /auth/enrollment)
             raise
