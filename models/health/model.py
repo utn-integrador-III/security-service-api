45e0ea69c7ff56082a0cccb2724d1b6f1ef93d97
chrisOMG33 <christopherce1918@gmail.com>
2024-07-12 14:51:30 -0600

    Cambios dejados por defecto

--- models/health/model.py
+++ models/health/model.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from models.health.db_queries import __dbmanager__
 
 
@@ -18,25 +17,4 @@ class HealthModel():
                 info_db.append(info)
             except Exception as ex:
                 raise Exception(ex)
-=======
-from models.health.db_queries import __dbmanager__
-
-
-# ================================================================
-# D A T A A C C E S S C O D E
-# ================================================================
-# Create connection with MongoDb
-
-class HealthModel():
-
-    # @classmethod
-    def getInfoDB():
-        info_db = []
-        response = __dbmanager__.get_all_data()
-        for info in response:
-            try:
-                info_db.append(info)
-            except Exception as ex:
-                raise Exception(ex)
->>>>>>> 3700107 (Document the endpoint to enroll a new user. In swagger.yml add the endpoint inside Auth section & the existing endpoint with the path /register was changed to the correct /auth/enrollment)
         return info_db
