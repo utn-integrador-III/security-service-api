e9ba60038cff85065930e08da742a00cd177552b
dferreto <diferretomo@est.utn.ac.cr>
2024-07-12 20:29:42 -0600

    Habilitar en endpoint de autenticación que permita la verificación del password encriptado en la BD

--- models/auth/auth.py
+++ models/auth/auth.py
@@ -1,10 +1,11 @@
 from models.auth.db_queries import __dbmanager__
+from utils.encryption_utils import EncryptionUtil
 
 def find_user_by_email(email):
     user = __dbmanager__.collection.find_one({"email": email})
     return user
-class UserModel:
 
+class UserModel:
     @staticmethod
     def find_by_email(email):
         user = find_user_by_email(email)
@@ -12,4 +13,6 @@ class UserModel:
 
     @staticmethod
     def verify_password(input_password, stored_password):
-        return input_password == stored_password
+        encryption_util = EncryptionUtil()   
+        decrypted_stored_password = encryption_util.decrypt(stored_password)
+        return input_password == decrypted_stored_password
