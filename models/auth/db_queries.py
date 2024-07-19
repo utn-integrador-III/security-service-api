e275f583da0630cb8c8dea4d1d986138b74951bd
Joxini <jocksanvargas15@gmail.com>
2024-07-18 23:26:19 -0600

    Correccion de errores

--- models/auth/db_queries.py
+++ models/auth/db_queries.py
@@ -3,3 +3,11 @@ from decouple import config
 
 __dbmanager__ = Connection(config("USER_COLLECTION"))
 
+def find_by_email(email):
+    user = __dbmanager__.collection.find_one({"email": email})
+    if user:
+        user['id'] = str(user.pop('_id'))  # Convert ObjectId to string and assign it to 'id'
+    return user
+
+def update_by_condition(condition, update_values):
+    __dbmanager__.collection.update_one(condition, {'$set': update_values})
